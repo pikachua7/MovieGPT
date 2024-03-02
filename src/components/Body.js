58ec3b7e18896d5c579245e12eddddef000111e1
pikachua7 <atharvapaliwal7@gmail.com>
2024-03-02 17:49:33 +0530

    add routers

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,11 +1,22 @@
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import { Browse } from "./Browse";
 import { Login } from "./Login";
 
 export const Body = () => {
+  const appRouter = createBrowserRouter([
+    {
+      path: "/",
+      element: <Login />,
+    },
+    {
+      path: "/browse",
+      element: <Browse />,
+    },
+  ]);
+
   return (
     <div>
-      <Login />
-      <Browse />
+      <RouterProvider router={appRouter} />
     </div>
   );
 };
