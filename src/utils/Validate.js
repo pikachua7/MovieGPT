d7c2bc47e753b02f9b65258154e3bc43483f34bc
pikachua7 <atharvapaliwal7@gmail.com>
2024-03-03 16:53:25 +0530

    update email validation logic

--- src/utils/Validate.js
+++ src/utils/Validate.js
@@ -1,9 +1,8 @@
 export const checkValidData = (email, password) => {
-  const isEmailValid =
-    /^[a-zA-Z0-9. _-]+@[a-zA-Z0-9. -]+\. [a-zA-Z]{2,4}$/.test(email);
+  const isEmailValid = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/.test(email);
   const isPasswordValid =
     /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/.test(password);
-  if (!isEmailValid || !isPasswordValid)
-    return "Email Id or Password is not valid";
-  else return true;
+  if (!isEmailValid) return "Email Id is not valid";
+  else if (!isPasswordValid) return "Password is not valid";
+  else return "Validation Successful!";
 };
